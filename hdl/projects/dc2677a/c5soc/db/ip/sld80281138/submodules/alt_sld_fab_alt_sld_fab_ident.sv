6dc7f1cc20e6ef27c1844e7cde22b155b8a1a7fc
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 16:45:44 +0800

    add: data, valid, en signals
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b99fcfa43b8dc6a7f4ad
+//   DESIGN_HASH   : 931a507dbfd765a53785
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb99fcfa43b8dc6a7f4ad };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h931a507dbfd765a53785 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
