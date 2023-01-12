2529fd2f84de158d4499e42ddfe7420e4392b371
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 19:14:42 +0800

    put: internal signals to debug cmos if
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 9b17cfef10e6c378598a
+//   DESIGN_HASH   : 66390d8eec679bc440d9
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9b17cfef10e6c378598a };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h66390d8eec679bc440d9 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
