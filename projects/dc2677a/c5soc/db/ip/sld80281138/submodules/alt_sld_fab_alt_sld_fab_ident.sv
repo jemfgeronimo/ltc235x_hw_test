8a98dd8d00482ac847f8fc6387f7e79e315568a4
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 09:29:42 +0800

    change: trigger condition to continuous
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : fb8b1d139c774e3256e5
+//   DESIGN_HASH   : 38e176ebcc3d0fce9af0
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfb8b1d139c774e3256e5 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h38e176ebcc3d0fce9af0 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
