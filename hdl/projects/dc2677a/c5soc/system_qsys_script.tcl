c19ff8449d5f57d2921f266cabb19ab6114041a8
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 12:00:06 +0800

    create a directory hdl that consists of all the files
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

---
+++ hdl/projects/dc2677a/c5soc/system_qsys_script.tcl
@@ -0,0 +1,13 @@
+set project_name dc2677a_c5soc
+set mmu_enabled 1
+set ad_hdl_dir /home/guest/jemfgeronimo_workspace/ltc235x_hw_test/hdl
+package require qsys
+set_module_property NAME {system_bd}
+set_project_property DEVICE_FAMILY {Cyclone V}
+set_project_property DEVICE 5CSXFC6D6F31C8ES
+foreach {param value} {} { set ad_project_params($param) $value }
+source system_qsys.tcl
+set_interconnect_requirement {$system} {qsys_mm.maxAdditionalLatency} {4}
+set_interconnect_requirement {$system} {qsys_mm.clockCrossingAdapter} {AUTO}
+set_interconnect_requirement {$system} {qsys_mm.burstAdapterImplementation} {PER_BURST_TYPE_CONVERTER}
+save_system {system_bd.qsys}
