ee0af6fbfc859810584880eb9ca8668e3f091441
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 18:05:39 +0800

    add: data_store signal for debug
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 04f419b3d44aa5b34032
+//   DESIGN_HASH   : 9b17cfef10e6c378598a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h04f419b3d44aa5b34032 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9b17cfef10e6c378598a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
