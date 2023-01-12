0ad70d485eac667c328fb935594b974da0fbee9a
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-12 17:40:06 +0800

    add: ch_id signals for debugging
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 931a507dbfd765a53785
+//   DESIGN_HASH   : 04f419b3d44aa5b34032
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h931a507dbfd765a53785 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h04f419b3d44aa5b34032 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
