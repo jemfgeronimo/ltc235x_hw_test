5615419a46d2d86745243a3aafa0eb4f72bc2cee
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-13 10:31:42 +0800

    friday the 13th
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f1e053ac99e19d2c58cd
+//   DESIGN_HASH   : f2f05da4a41487e23503
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf1e053ac99e19d2c58cd };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf2f05da4a41487e23503 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
