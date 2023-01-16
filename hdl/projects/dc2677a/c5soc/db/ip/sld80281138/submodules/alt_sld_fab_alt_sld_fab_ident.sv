636e89004ec01097167defadfbc851f8665bf6a5
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-16 10:38:50 +0800

    check: db_i to adc_lane functionality
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8871bdce2c4e1b2853f9
+//   DESIGN_HASH   : 3270b870f9d93be271c2
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8871bdce2c4e1b2853f9 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h3270b870f9d93be271c2 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
