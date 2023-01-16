afedcca8d9ccfbb39be2065ebaebcadf691337f5
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-16 08:34:53 +0800

    check: sdo and db_i consistency
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f2f05da4a41487e23503
+//   DESIGN_HASH   : b987cfa19416fb49d217
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf2f05da4a41487e23503 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb987cfa19416fb49d217 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
