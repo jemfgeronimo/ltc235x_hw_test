b44a07e993ffe524461c9b240b6971427edccd4c
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-17 10:33:05 +0800

    add: adc_lane signal
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hdl/projects/dc2677a/c5soc/db/ip/sld80281138/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : c96a9a88e0979dc4fab6
+//   DESIGN_HASH   : 0d8a69159b3dbbd8c0d8
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc96a9a88e0979dc4fab6 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h0d8a69159b3dbbd8c0d8 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
