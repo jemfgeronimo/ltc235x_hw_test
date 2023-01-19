45c45c7f015993a429fb4ad5c0732baeeecdbdef
John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>
2023-01-19 17:04:38 +0800

    add: debug signals for scki functionality
    Signed-off-by: John Erasmus Mari F. Geronimo <johnerasmusmari.geronimo@analog.com>

--- hdl/projects/dc2677a/c5soc/system_bd/synthesis/submodules/axi_ltc235x_cmos.v
+++ hdl/projects/dc2677a/c5soc/system_bd/synthesis/submodules/axi_ltc235x_cmos.v
@@ -317,6 +317,26 @@ module axi_ltc235x_cmos #(
       adc_lane_7 <= {adc_lane_7[BW-2:0], db_i_d[7], db_i[7]};
     end
   end
+  reg         [BW:0]  adc_lane_0_debug;
+  reg         [BW:0]  adc_lane_1_debug;
+  reg         [BW:0]  adc_lane_2_debug;
+  reg         [BW:0]  adc_lane_3_debug;
+  reg         [BW:0]  adc_lane_4_debug;
+  reg         [BW:0]  adc_lane_5_debug;
+  reg         [BW:0]  adc_lane_6_debug;
+  reg         [BW:0]  adc_lane_7_debug;
+  always @(posedge scki) begin
+    if (scki != scki_d) begin
+      adc_lane_0_debug <= {adc_lane_0_debug[BW-1:0], db_i[0]};
+      adc_lane_1_debug <= {adc_lane_1_debug[BW-1:0], db_i[1]};
+      adc_lane_2_debug <= {adc_lane_2_debug[BW-1:0], db_i[2]};
+      adc_lane_3_debug <= {adc_lane_3_debug[BW-1:0], db_i[3]};
+      adc_lane_4_debug <= {adc_lane_4_debug[BW-1:0], db_i[4]};
+      adc_lane_5_debug <= {adc_lane_5_debug[BW-1:0], db_i[5]};
+      adc_lane_6_debug <= {adc_lane_6_debug[BW-1:0], db_i[6]};
+      adc_lane_7_debug <= {adc_lane_7_debug[BW-1:0], db_i[7]};
+    end
+  end
   
 
   // store the data from the rx buffers when all bits are received
